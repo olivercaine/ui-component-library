967a734c4162ae3011e3f57cc8256909e5af9d97
Oliver Caine <olliecaine@gmail.com>
2023-04-14 03:16:06 +1000

    Storybook story interactions

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,17 @@ const config = {
     builder: 'webpack5'
   },
   stories: ["../src/**/*.stories.tsx"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-postcss", "@storybook/addon-actions"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-actions",
+    "@storybook/addon-essentials",
+    "@storybook/addon-postcss",
+    "@storybook/addon-interactions"
+  ],
+  features: {
+    interactionsDebugger: true, // Enable playback controls
+    // storyStoreV7: true, // https://storybook.js.org/docs/react/configure/webpack#bundle-splitting
+  },
   // @ts-expect-error
   docs: {
     autodocs: true
