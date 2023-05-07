1a4238d8e1e6a818770a542bc280a0d51ed2b985
Oliver Caine <olliecaine@gmail.com>
2023-05-08 02:17:23 +1000

    Go to a story

--- src/ui/basics/Button.stories.tsx
+++ src/ui/basics/Button.stories.tsx
@@ -30,4 +30,4 @@ CustomText.play = async ({ canvasElement, args }) => {
   })
 }
 
-export const LinkToStory = template({ onClick: linkTo('Components/CustomComponent', 'Default'), text: 'Go to story' })
+export const LinkToStory = template({ onClick: linkTo('Components/CustomComponent', 'Default'), text: 'Go to a story' })
